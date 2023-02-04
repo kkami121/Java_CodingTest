18e05e7ba32c782eb9048a049244c6c2b40ce20f
kkami121 <kkami121@naver.com>
2023-02-04 18:07:45 +0900

    2023/02/04

---
+++ Lv_0/SumOfConsecutiveNumber.java
@@ -0,0 +1,32 @@
+package Lv_0;
+// 프로래머스 Lv_0 연속된 수의 합
+public class SumOfConsecutiveNumber {
+    public static void main(String[] args){
+        int num = 5;
+        int total = 10;
+
+        int [] answer = new int[num];
+
+        for(int i = -(total + num); i <= (total + num); i++){
+            int index = 0;
+            int sum = 0;
+            for(int j = i; j < (i + num); j++){
+                sum += j;
+                answer[index++] = j;
+            }å
+            if(sum == total){
+                break;
+            }
+        }
+        
+        for(int i = 0; i < num; i++){
+            System.out.println(answer[i]);
+        }
+    }
+}
+/*
+Tip
+1. answer값이 음수 일 수 있기 때문에 범위를 -(total + num)부터 설정
+2. 현재 i에서 1씩 늘어나면서 num 개수 만큼 sum을 구하기 위해서 (i + num)으로 범위 설정
+3. index와 sum을 첫 for 안에 초기화
+ */
---
+++ Lv_0/SumOfConsecutiveNumber2.java
@@ -0,0 +1,30 @@
+// 프로그래머스 Lv_0 연속된 수의 합(다른 풀이)
+public class SumOfConsecutiveNumber2 {
+    public static void main(String[] args){
+        int num = 5;
+        int total = 15;
+
+        // 방법1
+        int[] answer = new int[num];
+        int temp = 0;
+        for(int i=0;i<num;i++){
+            temp+=i;
+            System.out.println(temp);
+        }       
+        int value1 = (total-temp)/num;
+
+        for(int i=0;i<num;i++){
+            answer[i]=i+value1;             
+        }
+
+        // 방법2
+        int[] answer2 = new int[num];
+        int check = num*(num+1) / 2;
+        int value2 = (total-check)/num;
+
+        for(int i=1;i<num + 1;i++){
+            answer2[i-1]=i+value2;           
+        }
+        
+    }
+}
